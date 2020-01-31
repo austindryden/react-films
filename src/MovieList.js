8f8853adb723026b32a6ae8cb4243a438442e625
Austin Dryden <austinisnorobot@gmail.com>
2020-01-31 13:33:34 -0500

    moar movies

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 
 function MovieList(){
 
-    
+    return(
+        <ul>
+            <li>The Witch</li>
+            <li>The Lighthouse</li>
+            <li>Parasite</li>
+            <li>The Dead Don't Die</li>
+            <li>It Follows</li>
+        </ul>
+
+    );
 }
 
 export default MovieList;
