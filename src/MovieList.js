c55eded0ea630c1ee434a831fd5d9080b2657dc3
Austin Dryden <austinisnorobot@gmail.com>
2020-01-31 15:04:56 -0500

    more tings

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,19 +1,32 @@
 import React, { useLayoutEffect } from "react";
 
-function MovieList(){
+
+
+function _handleClick(){
+    console.log("clicky clicky");
+}
+
+function _handleMouseOver(){
+    console.log("thingy has been moused over");
+}
+
+class MovieList extends React.Component {
+
+render(){
 
     return(
         <ul>
-            <li>The Witch</li>
-            <li>The Lighthouse</li>
+            <li onClick={_handleClick}>The Witch</li>
+            <li onMouseOver={_handleMouseOver} >The Lighthouse</li>
             <li>Parasite</li>
             <li>The Dead Don't Die</li>
             <li>It Follows</li>
             <li>The Fifth Element</li>
             <li>John Carpenters Halloween</li>
+            <li>Drive</li>
         </ul>
 
     );
 }
-
+}
 export default MovieList;
--- src/index.js
+++ src/index.js
@@ -1,90 +1,95 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
 
-class Hero{
-    //properties or attributes
-    //"variables that belong to each instance"
-    constructor(health=18, power=5){
-        this.health = health;
-        this.power = power;
-    }
-    //behaviors or methods
-    //"function that belong to each instance"
-    hug(otherHero){
-        otherHero.recieveHealth(this.power);
+
+
+
+
+
+
+// class Hero{
+//     //properties or attributes
+//     //"variables that belong to each instance"
+//     constructor(health=18, power=5){
+//         this.health = health;
+//         this.power = power;
+//     }
+//     //behaviors or methods
+//     //"function that belong to each instance"
+//     hug(otherHero){
+//         otherHero.recieveHealth(this.power);
         
-    }
+//     }
 
-    recieveHealth(amount){
-        this.health += amount;
-        console.log(`Hero has gained health! new amount : ${this.health}`);
-    }
-}
+//     recieveHealth(amount){
+//         this.health += amount;
+//         console.log(`Hero has gained health! new amount : ${this.health}`);
+//     }
+// }
 
-class Cat{
-    constructor(name="cat", attack=10, health=20 ){
-        this.name = name;
-        this.attack = attack;
-        this.health = health;
-    }
+// class Cat{
+//     constructor(name="cat", attack=10, health=20 ){
+//         this.name = name;
+//         this.attack = attack;
+//         this.health = health;
+//     }
 
-    scratch(thing){
-        thing.takeDamage(this.attack);
-    }
+//     scratch(thing){
+//         thing.takeDamage(this.attack);
+//     }
 
-    takeDamage(amount){
-        this.health -= amount;
-        console.log(`${this.name} took ${amount} damage`);
+//     takeDamage(amount){
+//         this.health -= amount;
+//         console.log(`${this.name} took ${amount} damage`);
 
-    }
-}
+//     }
+// }
 
-class Car{
-    constructor(make="Volvo", model="740", year="1985"){
-        this.make=make;
-        this.model=model;
-        this.year=year;
-        this.mileage=0;
-    }
+// class Car{
+//     constructor(make="Volvo", model="740", year="1985"){
+//         this.make=make;
+//         this.model=model;
+//         this.year=year;
+//         this.mileage=0;
+//     }
 
-    drive(miles){
-        this.mileage+=miles;
-    }
+//     drive(miles){
+//         this.mileage+=miles;
+//     }
 
-    display(){
-        return `${this.year} ${this.make} ${this.model} has ${this.mileage} miles`;
+//     display(){
+//         return `${this.year} ${this.make} ${this.model} has ${this.mileage} miles`;
 
-    }
+//     }
 
-    odometerFraud(amount){
-        this.mileage -= amount;
-    }
-}
+//     odometerFraud(amount){
+//         this.mileage -= amount;
+//     }
+// }
 
-const RSP = new Car();
-const nu3 = new Car("Mazda", "3", "2007");
-const leBaron = new Car("Chrystler", "LeBaron", "1986");
-const aHero = new Hero();
-const anotherHero = new Hero();
-console.log(aHero);
-console.log(anotherHero);
-aHero.hug(anotherHero);
-aHero.hug(anotherHero);
+// const RSP = new Car();
+// const nu3 = new Car("Mazda", "3", "2007");
+// const leBaron = new Car("Chrystler", "LeBaron", "1986");
+// const aHero = new Hero();
+// const anotherHero = new Hero();
+// console.log(aHero);
+// console.log(anotherHero);
+// aHero.hug(anotherHero);
+// aHero.hug(anotherHero);
 
-let newCat = new Cat("babby");
-let otherCat = new Cat("baby G");
+// let newCat = new Cat("babby");
+// let otherCat = new Cat("baby G");
 
-newCat.scratch(otherCat);
-RSP.drive(500);
-console.log(RSP.display());
-
+// newCat.scratch(otherCat);
+// RSP.drive(500);
+// console.log(RSP.display());
